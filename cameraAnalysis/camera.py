31da00de81628ac6bc67ab90688604b64dd0e134
kkorzen <konradkorz61@gmail.com>
2023-11-02 12:00:36 +0100

    Clean project directory and rewrite test.py as an app.py

--- cameraAnalysis/camera.py
+++ cameraAnalysis/camera.py
@@ -1,15 +1,10 @@
 from picamera2 import Picamera2, Preview
-import time,libcamera
+import time
 
 def TakeAShot():
 	picam = Picamera2()
-
 	config = picam.create_preview_configuration(main={"size":(1600,1200)})
-	# config["transform"] = libcamera.Transform(hflip=1,vflip=1)
 	picam.configure(config)
-
 	picam.start()
-
-	picam.capture_file("camphoto.jpg")
-
+	picam.capture_file("RPi-camphoto.jpg")
 	picam.close()
--- test.py
+++
@@ -1,9 +0,0 @@
-from ImageAnalyzer import ImageAnalyzer as IA
-
-path = "picture.jpg"
-ia = IA()
-ia.PassImage(path)
-rgbs = ia.GetAverageRGB()
-with open("rgbs.txt", "w") as file:
-    for key, val in rgbs.items():
-        file.write(f"{key}, {val}\n")
